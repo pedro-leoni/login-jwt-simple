d3c2a1dc7abb3bbd2a0a5c2eab1468a676b6cc9b
pedro-leoni <pedroleoni1996@gmail.com>
2022-07-29 11:30:56 -0300

    fixing cors

--- api/src/app.js
+++ api/src/app.js
@@ -14,12 +14,15 @@ server.use(express.json({limit: '50mb'}));
 server.use(cookieParser());
 // server.use(morgan('dev'));
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next(); 
+  });
 
 server.use('/', routes);
 
